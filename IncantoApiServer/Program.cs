7cf1eaf2af1724b57f728e66ba86b55c45c0ba42
wata548 <watashimokia@gmail.com>
2026-04-15 09:20:08 +0900

    Change api name

--- IncantoApiServer/Program.cs
+++ IncantoApiServer/Program.cs
@@ -1,20 +1,23 @@
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using Account;
 using IncantoApiServer.UpdateLogic;
 using MatchMaking;
 using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.Pkcs;
 using Redis;
 using StackExchange.Redis;
 
 public class Program {
 
-	public static void ApiSetUp(WebApplication pApp) {
+	private static void ApiSetUp(WebApplication pApp) {
 		pApp.MapGet("/", () => "Hello?")
 			.WithName("GetWeatherForecast");
 		pApp.MapPost("/2fa", async (Account.Account pAccount, SignUpInfo pInfo) =>
 			await pAccount.SendMail(pInfo.Mail));
-		pApp.MapPost("/signIn", async (Account.Account pAccount, SignUpInfo pInfo) =>
+		pApp.MapPost("/SignIn", async (Account.Account pAccount, SignUpInfo pInfo) =>
 			await pAccount.SignIn(pInfo.Mail, pInfo.PassWord));
-		pApp.MapPost("/signUp", async (Account.Account pAccount, SignUpInfo pInfo ) =>
+		pApp.MapPost("/SignUp", async (Account.Account pAccount, SignUpInfo pInfo ) =>
 			await pAccount.SignUp(pInfo.Name, pInfo.Mail, pInfo.PassWord, pInfo.TwoFactorAuth));
 		pApp.MapPost("/JoinMatch", async (MatchSystem pMatch, AccountToken pToken) =>
 			await pMatch.Enter(pToken));
@@ -22,7 +25,7 @@ public class Program {
 			pMatch.Exit(pToken));
 	}
 
-	public static void SetSingleton(WebApplicationBuilder pBuilder) {
+	private static void SetSingleton(WebApplicationBuilder pBuilder) {
 		pBuilder.Services.AddSingleton<UpdateManager>();
 		pBuilder.Services.AddSingleton<RateLimitService>(provider => {
 			var redis = ConnectionMultiplexer.Connect(
@@ -40,13 +43,23 @@ public class Program {
 		pBuilder.Services.AddSingleton<MatchSystem>();
 		pBuilder.Services.AddHostedService<UpdateLoop>();
 	}
+
+	private static void Certification(WebApplicationBuilder pBuilder) {
+
+		pBuilder.WebHost.ConfigureKestrel(options => {
+			options.Listen(IPAddress.Any, 7272, listenOptions =>
+				listenOptions.UseHttps("Certification/certificate.pfx", Setting.Setting.Get("CertPassword"))
+			);
+		});
+	}
     
 	public static void Main(string[] args) {
 		Setting.Setting.Setup();
 		var builder = WebApplication.CreateBuilder(args);
 		builder.Services.AddEndpointsApiExplorer();
 		builder.Services.AddSwaggerGen();
-		
+	
+		Certification(builder);
 		SetSingleton(builder);
 		var app = builder.Build();
 
--- IncantoApiServer/Redis/Result.cs
+++ IncantoApiServer/Redis/Result.cs
@@ -1,6 +1,4 @@
 ﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-
 namespace Redis;
 
 public enum Status {
